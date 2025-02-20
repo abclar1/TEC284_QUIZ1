cfffd506d97ae557e568fc21589214bf9c9d4114
Alex Clark <alexbclark27@gmail.com>
2025-02-20 14:39:11 -0600

    Don't know why i cant understand RGB not RYG...

--- rgbScript/rgbScript.ino
+++ rgbScript/rgbScript.ino
@@ -1,13 +1,24 @@
-const int redButton = 7, yellowButton = 6, greenButton = 5;
+const int redButton = 7, greenButton = 6, blueButton = 5;
+int redButtonState = 0, greenButtonState = 0, blueButtonState = 0; 
 
 void setup() {
   Serial.begin(9600);
   pinMode(redButton, INPUT_PULLUP);
+  pinMode(blueButton, INPUT_PULLUP);
   pinMode(greenButton, INPUT_PULLUP);
-  pinMode(yellowButton, INPUT_PULLUP);
+
+  pinMode(redButton, OUTPUT);
+  pinMode(blueButton, OUTPUT);
+  pinMode(greenButton, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  redButtonState = digitalRead(2);
+  blueButtonState = digitalRead(2);
+  greenButtonState = digitalRead(2);
+  flashRed();
+}
 
+void flashRed(){
+  digitalWrite(redButton, HIGH);
 }
