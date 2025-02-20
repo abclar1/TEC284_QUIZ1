bc79542a302298ebc001bbd6b0dfac347edc9f0f
Alex Clark <alexbclark27@gmail.com>
2025-02-20 15:15:26 -0600

    Final Commit

--- rgbScript/rgbScript.ino
+++ rgbScript/rgbScript.ino
@@ -1,24 +1,52 @@
+//set some constant integers to store the pin numbers on the Uno
 const int redButton = 7, greenButton = 6, blueButton = 5;
-int redButtonState = 0, greenButtonState = 0, blueButtonState = 0; 
+const int redLight = 12, greenLight = 11, blueLight = 10;
+int redButtonState = 0, greenButtonState = 0, blueButtonState = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //silly rabbit, serial.begin is for kids
+
+  //set the buttons as inputs from the uno
   pinMode(redButton, INPUT_PULLUP);
   pinMode(blueButton, INPUT_PULLUP);
   pinMode(greenButton, INPUT_PULLUP);
 
-  pinMode(redButton, OUTPUT);
-  pinMode(blueButton, OUTPUT);
-  pinMode(greenButton, OUTPUT);
+  //set the led as outputs
+  pinMode(redLight, OUTPUT);
+  pinMode(blueLight, OUTPUT);
+  pinMode(greenLight, OUTPUT);
 }
 
 void loop() {
-  redButtonState = digitalRead(2);
-  blueButtonState = digitalRead(2);
-  greenButtonState = digitalRead(2);
-  flashRed();
+  //call the methods
+  setStates();
+  colorControl();
 }
 
-void flashRed(){
-  digitalWrite(redButton, HIGH);
+void setStates(){
+  //set the button state to the global variables
+  redButtonState = digitalRead(redButton);
+  greenButtonState = digitalRead(greenButton);
+  blueButtonState = digitalRead(blueButton); 
+}
+
+void colorControl(){
+  //if the button is pressed, turn the light the respective color, otherwise, turn it off
+  if (redButtonState == LOW) {
+    digitalWrite(redLight, HIGH);
+  } else {
+    digitalWrite(redLight, LOW);
+  }
+
+  if (greenButtonState == LOW) {
+    digitalWrite(greenLight, HIGH);
+  } else {
+    digitalWrite(greenLight, LOW);
+  }
+
+  if (blueButtonState == LOW) {
+    digitalWrite(blueLight, HIGH);
+  } else {
+    digitalWrite(blueLight, LOW);
+  }
 }
